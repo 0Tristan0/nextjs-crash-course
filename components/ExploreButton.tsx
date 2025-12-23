acd9a4b09408d87a7352224cf059c63985389f65
Tristan Goehring <goehrint@oregonstate.edu>
2025-12-23 09:51:22 -0800

    Set up hompage and post hog

--- components/ExploreButton.tsx
+++ components/ExploreButton.tsx
@@ -1,10 +1,17 @@
 "use client"
 
 import Image from "next/image"
+import posthog from "posthog-js"
 
 const ExploreButton = () => {
+  const handleExploreClick = () => {
+    posthog.capture('explore_events_clicked', {
+      button_location: 'hero_section',
+    });
+  };
+
   return (
-    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log("Explore button clicked")}>
+    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={handleExploreClick}>
         <a href="#events">
             Explore Events
             <Image src="/icons/arrow-down.svg" alt="Down Arrow" width={24} height={24}/>
